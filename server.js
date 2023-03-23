a808a66f4746f354d05e05c258bf95a3562878fe
iammohit8 <immohitsharma8@gmail.com>
2023-03-23 18:08:13 +0530

    /stock endpoint added

--- server.js
+++ server.js
@@ -6,9 +6,72 @@ const csv = require('fast-csv');
 const app = express();
 const csvHeader = 'name,password\n';
 const csvFilePath = 'login.csv';
+let stockData =
+[
+  {
+    stock: 'nifty',
+    entryType: 'US',
+    entryPrice: '1',
+    stopLoss: '1',
+    targetPoint: '1',
+    quantity: '1',
+    tag: 'US',
+    autoSquare: '1'
+  },
+  {
+    stock: 'nifty',
+    entryType: 'CA',
+    entryPrice: '11',
+    stopLoss: '11',
+    targetPoint: '11',
+    quantity: '11',
+    tag: 'CA',
+    autoSquare: '11'
+  },
+  {
+    stock: 'nifty',
+    entryType: 'FR',
+    entryPrice: '12',
+    stopLoss: '12',
+    targetPoint: '12',
+    quantity: '12',
+    tag: 'CA',
+    autoSquare: '12'
+  },
+  {
+    stock: 'nifty',
+    entryType: 'FR',
+    entryPrice: 'we',
+    stopLoss: '13',
+    targetPoint: '13',
+    quantity: '13',
+    tag: 'FR',
+    autoSquare: '13'
+  },
+  {
+    stock: 'nifty',
+    entryType: 'DE',
+    entryPrice: '14',
+    stopLoss: '14',
+    targetPoint: '14',
+    quantity: '14',
+    tag: 'DE',
+    autoSquare: '14'
+  },
+  {
+    stock: 'nifty',
+    entryType: 'DE',
+    entryPrice: 'we',
+    stopLoss: '12',
+    targetPoint: '435',
+    quantity: '435',
+    tag: 'DE',
+    autoSquare: '14'
+  }
+];
 
 app.use(cors({
-    origin:'http://192.168.200.197:4200', 
+    origin:'http://localhost:4200', 
     optionSuccessStatus:200,
  }
  ));
@@ -48,6 +111,23 @@ app.get('/data', (req,res) => {
         })
     })
 
+ app.post('/stock', (req,res)=>{
+        stockData.push(req.body);
+        console.log(stockData);
+        res.status(200).json({status: 'success', message: 'stock data saved successfully'});
+        
+    
+
+});
+
+app.get('/stock', (req,res)=>{
+
+    
+    res.status(200).json({status: 'success', data: stockData});
+    
+
+
+});
 
 
 const server = app.listen(3000, () => {
