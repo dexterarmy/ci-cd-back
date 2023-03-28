cbe564c63948c05672a54fd6ebd27fe4ece7fe16
iammohit8 <immohitsharma8@gmail.com>
2023-03-28 18:28:40 +0530

    data

--- server.js
+++ server.js
@@ -6,69 +6,7 @@ const csv = require('fast-csv');
 const app = express();
 const csvHeader = 'name,password\n';
 const csvFilePath = 'login.csv';
-let stockData =
-[
-  {
-    stock: 'nifty',
-    entryType: 'US',
-    entryPrice: '1',
-    stopLoss: '1',
-    targetPoint: '1',
-    quantity: '1',
-    tag: 'US',
-    autoSquare: '1'
-  },
-  {
-    stock: 'nifty',
-    entryType: 'CA',
-    entryPrice: '11',
-    stopLoss: '11',
-    targetPoint: '11',
-    quantity: '11',
-    tag: 'CA',
-    autoSquare: '11'
-  },
-  {
-    stock: 'nifty',
-    entryType: 'FR',
-    entryPrice: '12',
-    stopLoss: '12',
-    targetPoint: '12',
-    quantity: '12',
-    tag: 'CA',
-    autoSquare: '12'
-  },
-  {
-    stock: 'nifty',
-    entryType: 'FR',
-    entryPrice: 'we',
-    stopLoss: '13',
-    targetPoint: '13',
-    quantity: '13',
-    tag: 'FR',
-    autoSquare: '13'
-  },
-  {
-    stock: 'nifty',
-    entryType: 'DE',
-    entryPrice: '14',
-    stopLoss: '14',
-    targetPoint: '14',
-    quantity: '14',
-    tag: 'DE',
-    autoSquare: '14'
-  },
-  {
-    stock: 'nifty',
-    entryType: 'DE',
-    entryPrice: 'we',
-    stopLoss: '12',
-    targetPoint: '435',
-    quantity: '435',
-    tag: 'DE',
-    autoSquare: '14'
-  }
-];
+
 
 app.use(cors({
     origin:'http://localhost:4200', 
@@ -112,8 +50,17 @@ app.get('/data', (req,res) => {
     })
 
  app.post('/stock', (req,res)=>{
-        stockData.push(req.body);
-        console.log(stockData);
+       
+        const header = `${Object.keys(req.body).join(',')}\n`;
+        const csv = `${Object.values(req.body).join(',')}\n`;
+        if(!fs.existsSync('stockData.csv') || !fs.readFileSync('stockData.csv').toString().includes(header)){
+          fs.writeFileSync('stockData.csv', header);
+      }
+
+      fs.appendFileSync('stockData.csv', csv);
+        // const data = fs.readFileSync('login.csv').toString();
+        // const foundData = data.slice(0, data.indexOf("\n"));
+       
         res.status(200).json({status: 'success', message: 'stock data saved successfully'});
         
     
@@ -122,8 +69,27 @@ app.get('/data', (req,res) => {
 
 app.get('/stock', (req,res)=>{
 
+  fs.readFile('stockData.csv', 'utf-8', (err,data) => {
+    if(err){
+        console.error(err);
+        return;
+    }
+
+    const stockData = [];
+    fs.createReadStream('stockData.csv')
+    .pipe(csv.parse({ headers: true }))
+    .on('data', row => stockData.push(row))
+     .on('end', () => {
+          //  console.log(names);
+       // Do something with the names
+       res.status(200).json({status : 'success', data: stockData});
+      });
+
+          
+    })
+
     
-    res.status(200).json({status: 'success', data: stockData});
+    // res.status(200).json({status: 'success'});
     
 
 
